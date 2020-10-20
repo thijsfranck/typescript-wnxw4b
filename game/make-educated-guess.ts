8915df555aab1e9e60098db7b4dd09d49bf77cd8
thijsfranck <thijsfranck@gmail.com>
2020-10-20 20:09:38 +0200

    1 file modified

--- game/make-educated-guess.ts
+++ game/make-educated-guess.ts
@@ -10,13 +10,12 @@ export function makeEducatedGuess(
   turns: Iterable<Turn>,
   solutionSpace: Set<string>
 ) {
-  let bullsCount = new DefaultMap<string, number>(() => 0);
-  let cowsCount = new DefaultMap<string, number>(() => 0);
+  let symbolWeight = new DefaultMap<string, number>(() => 0);
 
   for (const { bulls, cows, guess } of turns) {
     for (const key of guess) {
-      increment(bullsCount, key, bulls);
-      increment(cowsCount, key, cows);
+      const currentWeight = symbolWeight.get(key);
+      symbolWeight.set(key, currentWeight + bulls + cows);
     }
   }
 
@@ -25,8 +24,7 @@ export function makeEducatedGuess(
   for (const solution of solutionSpace) {
     let solutionScore = 0;
 
-    for (const key of solution)
-      solutionScore -= cowsCount.get(key) - bullsCount.get(key);
+    for (const key of solution) solutionScore -= symbolWeight.get(key);
 
     if (solutionScore === maxScore) alternatives.push(solution);
 
@@ -39,9 +37,4 @@ export function makeEducatedGuess(
   const guess = Math.floor(Math.random() * alternatives.length);
 
   return alternatives[guess];
-}
-
-function increment(map: DefaultMap<string, number>, key: string, amount = 1) {
-  const currentValue = map.get(key);
-  map.set(key, currentValue + amount);
 }
